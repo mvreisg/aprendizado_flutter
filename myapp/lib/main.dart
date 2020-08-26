9b6404ba6d5828220657824fa09a9cfa0c5e1e39
mvreisg <marcus_viniciusreis@hotmail.com>
2020-08-26 07:31:58 -0300

    end of tutorial 6

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Text('hey ninjas!'),
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        //specify what will be in the home screen when we load this app
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            'hello ninjas',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    ));
