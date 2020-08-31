38f32034d8c7c07e82e0b6f975bf03a98e479f0b
mvreisg <marcus_viniciusreis@hotmail.com>
2020-08-31 08:13:28 -0300

    end of tutorial 33

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -1,12 +1,13 @@
+import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
-import 'dart:convert';
 
 class WorldTime {
   String location; // location name for the UI
   String time; // the time in that location
   String flag; // url to an asset flag icon
   String url; // location url for api endpoint
+  bool isDayTime; // true or false if daytime or not
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -31,6 +32,7 @@ class WorldTime {
         ),
       );
       // set the time property
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     } catch (e) {
       print('caught error: $e');
